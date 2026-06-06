f7ccb96c1f226a3e5159f5775bac9c90b442fc04
Alexander Klepikov <sklepikov2007@gmail.com>
2026-06-06 16:32:38 +0600

    add button for new game

--- game/view.py
+++ game/view.py
@@ -1,9 +1,32 @@
+import tkinter as tk
+
+GEOMETRY = "600x600"
 
 
 class GameView:
     def __init__(self, root):
         self.root = root
-        self.create_ui()
+        self.create_main_menu()
 
-    def create_ui(self):
-        self.root.geometry("600x600")
+    def create_main_menu(self):
+        self.root.geometry(GEOMETRY)
+
+        title = tk.Label(
+            text="Battle of Teams",  # in eng - Battle of Teams
+            font=("Arial", 32, "bold"),
+        )
+        title.pack(pady=12)
+
+        # create buttons for main menu, use lamda-functions for comfort
+        button = tk.Button(
+            self.root, command=self.on_game_button_click, text="Start new game"
+        )
+        button.pack()
+
+        # create settings menu like a hierarchical menu
+
+    def on_game_button_click(self):
+        self.root.destroy()
+        self.window = tk.Tk()
+        self.window.geometry(GEOMETRY)
+        self.window.title("Active game window")
